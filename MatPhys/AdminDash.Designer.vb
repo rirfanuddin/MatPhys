42958f73054591b35ebf5c33bf950f9579e533da
Muhammad Ariq Srihadi Wibowoputra <muhammadariq13@gmail.com>
2018-05-03 12:49:14 +0700

    admindash base done

--- MatPhys/AdminDash.Designer.vb
+++ MatPhys/AdminDash.Designer.vb
@@ -26,14 +26,32 @@ Partial Class AdminDash
         Me.AccountToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.general_stats = New System.Windows.Forms.TabPage()
         Me.sd_stats = New System.Windows.Forms.TabPage()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.smp_stats = New System.Windows.Forms.TabPage()
         Me.sma_stats = New System.Windows.Forms.TabPage()
-        Me.general_stats = New System.Windows.Forms.TabPage()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.DataGridView3 = New System.Windows.Forms.DataGridView()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.DataGridView4 = New System.Windows.Forms.DataGridView()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
         Me.MenuStrip1.SuspendLayout()
         Me.TabControl1.SuspendLayout()
+        Me.general_stats.SuspendLayout()
         Me.sd_stats.SuspendLayout()
+        Me.smp_stats.SuspendLayout()
+        Me.sma_stats.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView4, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'MenuStrip1
@@ -41,7 +59,7 @@ Partial Class AdminDash
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AccountToolStripMenuItem, Me.HelpToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(456, 24)
         Me.MenuStrip1.TabIndex = 1
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -66,48 +84,34 @@ Partial Class AdminDash
         Me.TabControl1.Location = New System.Drawing.Point(13, 28)
         Me.TabControl1.Name = "TabControl1"
         Me.TabControl1.SelectedIndex = 0
-        Me.TabControl1.Size = New System.Drawing.Size(775, 410)
+        Me.TabControl1.Size = New System.Drawing.Size(431, 335)
         Me.TabControl1.SizeMode = System.Windows.Forms.TabSizeMode.FillToRight
         Me.TabControl1.TabIndex = 2
         '
+        'general_stats
+        '
+        Me.general_stats.Controls.Add(Me.DataGridView2)
+        Me.general_stats.Controls.Add(Me.TextBox2)
+        Me.general_stats.Controls.Add(Me.Label2)
+        Me.general_stats.Location = New System.Drawing.Point(4, 22)
+        Me.general_stats.Name = "general_stats"
+        Me.general_stats.Size = New System.Drawing.Size(423, 309)
+        Me.general_stats.TabIndex = 3
+        Me.general_stats.Text = "Statistik Umum"
+        Me.general_stats.UseVisualStyleBackColor = True
+        '
         'sd_stats
         '
+        Me.sd_stats.Controls.Add(Me.DataGridView1)
+        Me.sd_stats.Controls.Add(Me.TextBox1)
         Me.sd_stats.Controls.Add(Me.Label1)
         Me.sd_stats.Location = New System.Drawing.Point(4, 22)
         Me.sd_stats.Name = "sd_stats"
         Me.sd_stats.Padding = New System.Windows.Forms.Padding(3)
-        Me.sd_stats.Size = New System.Drawing.Size(767, 384)
+        Me.sd_stats.Size = New System.Drawing.Size(423, 309)
         Me.sd_stats.TabIndex = 0
         Me.sd_stats.Text = "Statistik - SD"
         Me.sd_stats.UseVisualStyleBackColor = True
-        '
-        'smp_stats
-        '
-        Me.smp_stats.Location = New System.Drawing.Point(4, 22)
-        Me.smp_stats.Name = "smp_stats"
-        Me.smp_stats.Padding = New System.Windows.Forms.Padding(3)
-        Me.smp_stats.Size = New System.Drawing.Size(767, 384)
-        Me.smp_stats.TabIndex = 1
-        Me.smp_stats.Text = "Statistik - SMP"
-        Me.smp_stats.UseVisualStyleBackColor = True
-        '
-        'sma_stats
-        '
-        Me.sma_stats.Location = New System.Drawing.Point(4, 22)
-        Me.sma_stats.Name = "sma_stats"
-        Me.sma_stats.Size = New System.Drawing.Size(767, 384)
-        Me.sma_stats.TabIndex = 2
-        Me.sma_stats.Text = "Statistik - SMA"
-        Me.sma_stats.UseVisualStyleBackColor = True
-        '
-        'general_stats
-        '
-        Me.general_stats.Location = New System.Drawing.Point(4, 22)
-        Me.general_stats.Name = "general_stats"
-        Me.general_stats.Size = New System.Drawing.Size(767, 384)
-        Me.general_stats.TabIndex = 3
-        Me.general_stats.Text = "Statistik Umum"
-        Me.general_stats.UseVisualStyleBackColor = True
         '
         'Label1
         '
@@ -118,11 +122,127 @@ Partial Class AdminDash
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Siswa Terdaftar: "
         '
+        'smp_stats
+        '
+        Me.smp_stats.Controls.Add(Me.DataGridView3)
+        Me.smp_stats.Controls.Add(Me.TextBox3)
+        Me.smp_stats.Controls.Add(Me.Label3)
+        Me.smp_stats.Location = New System.Drawing.Point(4, 22)
+        Me.smp_stats.Name = "smp_stats"
+        Me.smp_stats.Padding = New System.Windows.Forms.Padding(3)
+        Me.smp_stats.Size = New System.Drawing.Size(423, 309)
+        Me.smp_stats.TabIndex = 1
+        Me.smp_stats.Text = "Statistik - SMP"
+        Me.smp_stats.UseVisualStyleBackColor = True
+        '
+        'sma_stats
+        '
+        Me.sma_stats.Controls.Add(Me.DataGridView4)
+        Me.sma_stats.Controls.Add(Me.TextBox4)
+        Me.sma_stats.Controls.Add(Me.Label4)
+        Me.sma_stats.Location = New System.Drawing.Point(4, 22)
+        Me.sma_stats.Name = "sma_stats"
+        Me.sma_stats.Size = New System.Drawing.Size(423, 309)
+        Me.sma_stats.TabIndex = 2
+        Me.sma_stats.Text = "Statistik - SMA"
+        Me.sma_stats.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Location = New System.Drawing.Point(114, 25)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(68, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(24, 72)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(375, 231)
+        Me.DataGridView1.TabIndex = 3
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Location = New System.Drawing.Point(25, 62)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.Size = New System.Drawing.Size(375, 231)
+        Me.DataGridView2.TabIndex = 6
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Enabled = False
+        Me.TextBox2.Location = New System.Drawing.Point(115, 15)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(68, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(22, 18)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(87, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Siswa Terdaftar: "
+        '
+        'DataGridView3
+        '
+        Me.DataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView3.Location = New System.Drawing.Point(25, 62)
+        Me.DataGridView3.Name = "DataGridView3"
+        Me.DataGridView3.Size = New System.Drawing.Size(375, 231)
+        Me.DataGridView3.TabIndex = 6
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Enabled = False
+        Me.TextBox3.Location = New System.Drawing.Point(115, 15)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(68, 20)
+        Me.TextBox3.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(22, 18)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(87, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Siswa Terdaftar: "
+        '
+        'DataGridView4
+        '
+        Me.DataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView4.Location = New System.Drawing.Point(25, 62)
+        Me.DataGridView4.Name = "DataGridView4"
+        Me.DataGridView4.Size = New System.Drawing.Size(375, 231)
+        Me.DataGridView4.TabIndex = 6
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Enabled = False
+        Me.TextBox4.Location = New System.Drawing.Point(115, 15)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(68, 20)
+        Me.TextBox4.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(22, 18)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(87, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Siswa Terdaftar: "
+        '
         'AdminDash
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(456, 375)
         Me.Controls.Add(Me.TabControl1)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
@@ -131,8 +251,18 @@ Partial Class AdminDash
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
         Me.TabControl1.ResumeLayout(False)
+        Me.general_stats.ResumeLayout(False)
+        Me.general_stats.PerformLayout()
         Me.sd_stats.ResumeLayout(False)
         Me.sd_stats.PerformLayout()
+        Me.smp_stats.ResumeLayout(False)
+        Me.smp_stats.PerformLayout()
+        Me.sma_stats.ResumeLayout(False)
+        Me.sma_stats.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView4, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -146,4 +276,15 @@ Partial Class AdminDash
     Friend WithEvents sma_stats As TabPage
     Friend WithEvents general_stats As TabPage
     Friend WithEvents Label1 As Label
+    Friend WithEvents DataGridView2 As DataGridView
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents DataGridView3 As DataGridView
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents DataGridView4 As DataGridView
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label4 As Label
 End Class
