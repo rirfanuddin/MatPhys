8fa922e15e955dcb1197e5b8adc139848837eaa7
yusufnur <yusuf.n@mail.ugm.ac.id>
2018-05-17 13:00:29 +0700

    update rumus

--- MatPhys/Main.vb
+++ MatPhys/Main.vb
@@ -3,4 +3,11 @@
         Dim about = New AboutBox
         about.Show()
     End Sub
+
+    Private Sub TreeView1_AfterSelect(sender As Object, e As TreeViewEventArgs) Handles TreeView1.AfterSelect
+        If TreeView1.SelectedNode.Text = "Bangun Ruang" Then
+            rumusSdBangunRuang.Visible = True
+
+        End If
+    End Sub
 End Class
