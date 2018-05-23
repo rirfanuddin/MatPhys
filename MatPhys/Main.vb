458f47a002dcc34bdccd31efd086deb3778cb4fd
Muhammad Ariq Srihadi Wibowoputra <muhammadariq13@gmail.com>
2018-05-23 21:45:10 +0700

    user details, functional log out

--- MatPhys/Main.vb
+++ MatPhys/Main.vb
@@ -6,8 +6,17 @@
 
     Private Sub TreeView1_AfterSelect(sender As Object, e As TreeViewEventArgs) Handles TreeView1.AfterSelect
         If TreeView1.SelectedNode.Text = "Bangun Ruang" Then
-            rumusSdBangunRuang.Visible = True
+            rumusSdKubus.Visible = True
 
         End If
+    End Sub
+
+    Private Sub LogOutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LogOutToolStripMenuItem.Click
+        Me.Hide()
+        LoginForm.Show()
+    End Sub
+
+    Private Sub UserDetailsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles UserDetailsToolStripMenuItem.Click
+        UserDetails.Show()
     End Sub
 End Class
